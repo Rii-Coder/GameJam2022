c1cbd50d90c6df642b78c8c09a6ec919d5d62300
TheR2D2cx <jamv1305@gmail.com>
2022-09-29 23:03:05 -0700

    First Try (Testing)
    Background set and filled full screen.
    
    Implement new system of dialogs (need adapt with collections of dialogs).

---
+++ My project (1)/Assets/Dialogue.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public string[] lines;
+    public float textSpeed;
+
+    private int index;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = string.Empty;
+        startDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            if(text.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                text.text = lines[index];
+            }
+        }
+    }
+
+    void startDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach(char c in lines[index].ToCharArray())
+        {
+            text.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if(index < lines.Length - 1)
+        {
+            index++;
+            text.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
